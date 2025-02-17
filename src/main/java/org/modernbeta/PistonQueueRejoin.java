e6bb0e929fd64bdf6eb2b729c8c7e8c9d61c4fe9
Lynx <141365347+iLynxcat@users.noreply.github.com>
2025-02-17 16:37:05 -0600

    Get LuckPerms API on enable

--- src/main/java/org/modernbeta/PistonQueueRejoin.java
+++ src/main/java/org/modernbeta/PistonQueueRejoin.java
@@ -1,13 +1,26 @@
 package org.modernbeta;
 
+import net.luckperms.api.LuckPerms;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PistonQueueRejoin extends JavaPlugin implements Listener {
+    private LuckPerms luckPerms;
+
     @Override
     public void onEnable() {
+        RegisteredServiceProvider<LuckPerms> provider =
+            getServer().getServicesManager().getRegistration(LuckPerms.class);
+        if (provider == null) {
+            getLogger().severe("Couldn't get LuckPerms API provider!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        } else
+            luckPerms = provider.getProvider();
+
         getServer().getPluginManager().registerEvents(this, this);
     }
 
